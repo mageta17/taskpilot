51d58c8baea0934d71bdc4ebd88fdff4528425f3
mageta sukulu <magetasukulu@gmail.com>
2025-06-04 11:01:21 +0300

    Update

---
+++ backend/taskpilot/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
