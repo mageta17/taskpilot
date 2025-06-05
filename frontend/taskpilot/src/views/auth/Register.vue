82e14ee6866b45f2244aef6f198db98b96bf97f1
mageta sukulu <magetasukulu@gmail.com>
2025-06-05 10:16:51 +0300

    Update

--- frontend/taskpilot/src/views/auth/Register.vue
+++ frontend/taskpilot/src/views/auth/Register.vue
@@ -1,21 +1,32 @@
 <script setup lang="ts">
-import axios from "axios";
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+axios.defaults.baseURL = 'http://localhost:9000';
 
 const register = async () => {
-    console.log("Registering...");
+    console.log('Registering...');
+
+    await axios.get('/sanctum/csrf-cookie');
 
     try {
-        const response = await axios.post("http://localhost:3000/api/register", {
-            name: "John Doe",
-            email: "john@doe.com",
-            password: "password123",
-            password_confirmation: "password123"
-        });
+        const response = await axios.post(
+            '/api/register', 
+            {
+                name: 'John Doe',
+                email: 'john@doe.com',
+                password: 'password123',
+                password_confirmation: 'password123',
+            }
+        );
         console.log(response.data);
     } catch (error) {
         console.error(error);
     }
-}
+};
+
+register();
 </script>
 <template>
     <h1>Register</h1>
