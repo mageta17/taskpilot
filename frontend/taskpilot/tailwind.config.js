fb76e8795051c0bd015d9b52e93a8c13c4b47e74
mageta sukulu <magetasukulu@gmail.com>
2025-06-07 21:04:11 +0300

    Auto commit

--- frontend/taskpilot/tailwind.config.js
+++ frontend/taskpilot/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",                       // Scan your main HTML file
+    "./src/**/*.{vue,js,ts,jsx,tsx}"      // Scan all files in /src with these extensions
+  ],
   theme: {
     extend: {},
   },
--- frontend/taskpilot/vite.config.js
+++ frontend/taskpilot/vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -12,7 +11,14 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    host: true,             // Required so Vite listens on all network interfaces
+    watch: {
+      usePolling: true,     // Enables polling so file changes inside Docker are detected
+      interval: 300,        // Optional: milliseconds between polling
     },
   },
 })
